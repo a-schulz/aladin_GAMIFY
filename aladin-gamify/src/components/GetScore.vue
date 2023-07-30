14a72aa347e78f2c79554a669726534ccaddf22b
a-schulz <schulz.alexander2002@gmail.com>
2023-07-30 20:37:27 +0200

    zwischenstand, verschachteltes Object wird ohne vorgegebene (generische) Struktur erstellt

--- aladin-gamify/src/components/GetScore.vue
+++ aladin-gamify/src/components/GetScore.vue
@@ -20,147 +20,43 @@ const impactMethodsToScore = {
 };
 
 // Methode mit Paul absprechen ob es so auch generisch genug ist, damit hier dann der Score berechnet werden kann.
-interface IAttribute {
-  name: string,
-  timestamp: number,
-  value: string,
-  score?: number
-}
 
-interface IComponent {
-  name: number,
-  attribute: IAttribute[]
-  score?: number
-}
-
-interface IComponents {
-  attribute?: IAttribute,
-  component?: IComponent
-}
-
-interface INode {
-  nodeId: number,
-  components: { [key: string]: IComponents }
-  score?: number
-}
-
-interface INodes {
-  [key: string]: INode;
-}
-
-// const example : INodes = {
-//   "1": {
-//     id: 1,
-//     components: {
-//       "fillZeros": {
-//         attribute: {
-//           name: "fillZeros",
-//           timestamp: 123,
-//           value: "true",
-//           score: 0.5
-//         },
-//         component: {
-//           name: 1,
-//           attribute: [
-//             {
-//               name: "fillZeros",
-//               timestamp: 123,
-//               value: "true",
-//               score: 0.5
-//             }
-//           ],
-//           score: 0.5
-//         }
-//       },
-//       "showSolution": {
-//         attribute: {
-//           name: "showSolution",
-//           timestamp: 123,
-//           value: "true",
-//           score: 0
-//         },
-//         component: {
-//           name: 1,
-//           attribute: [
-//             {
-//               name: "showSolution",
-//               timestamp: 123,
-//               value: "true",
-//               score: 0
-//             }
-//           ],
-//           score: 0
-//         }
-//       },
-//       "copyToClipboard": {
-//         attribute: {
-//           name: "copyToClipboard",
-//           timestamp: 123,
-//           value: "true",
-//           score: 1
-//         },
-//         component: {
-//           name: 1,
-//           attribute: [
-//             {
-//               name: "copyToClipboard",
-//               timestamp: 123,
-//               value: "true",
-//               score: 1
-//             }
-//           ],
-//           score: 1
-//         }
-//       }
-//     },
-//     score: 0.5
-//   }
-// }
-
-const calculateScore = (steps: ISteps[]) => {
+const calculateScore = (object: Object) => {
   let result = 0;
-  // Schrittweise Berechnung pro Node und pro Component separat
-  // unteres ist dann jeweils die aggregation -> Datawarehouse??
-  const nodes = <INodes>{};
-  steps.forEach((step: ISteps) => {
-    if ((/nodes__\d+__components__\d+/).test(step.path)) {
-      const path = step.path.split("__");
-      nodes[path[1]] = {
-        nodeId: path[1],
-        components: {
-          [path[3]]: {},
-        },
-        score: 0,
-      };
-      // FixMe: Attribute werden aktuell noch überschrieben
-      if (!(/nodes__\d+__components__\d+__component/).test(step.path)) {
-        nodes[path[1]].components[path[3]].attribute = {
-          name: path[4],
-          timestamp: step.timestamp,
-          value: step.value,
-          score: 0,
-        };
-      } else {
-        console.log("component", path);
-        nodes[path[1]].components[path[3]].component = {
-          name: path[4],
-          attribute: [
-            {
-              name: path[5],
-              timestamp: step.timestamp,
-              value: step.value,
-              score: 0,
-            },
-          ],
-          score: 0,
-        };
-      }
-    }
-  });
-  console.log(nodes);
   score.value = result;
 };
 
+const steps2object = (steps: ISteps[]) => {
+  let result: Record<string, any> = {};
+  steps.forEach((step: ISteps) => {
+    // Split the inputString by "__"
+    const keys: string[] = step.path.split("__");
+    // Reference to the current object being modified
+    let currentObject: Record<string, any> = result;
+    // Iterate through the keys array and construct the nested object
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+
+      if (i === keys.length - 1) {
+        // If it's the last key, assign the value to the property
+        currentObject[key] = step.value;
+      } else {
+        // Otherwise, create a nested object and move to the next level
+        if (!currentObject[key]) {
+          currentObject[key] = {};
+        }
+        currentObject = currentObject[key];
+      }
+    }
+  });
+
+  return result;
+};
+
+console.log(steps2object(relevantSteps));
+
+// TODO: create mapping to define which paths from the object are necessary values for the score
+// Prüfen, muss ich die TimeStamps noch einfügen?
 </script>
 
 <template>
