b0c69dc9c025d7db21165e75b932d3a3da1d7408
a-schulz <schulz.alexander2002@gmail.com>
2023-07-31 17:52:51 +0200

    first calculation of the score but not finished

--- aladin-gamify/src/components/GetScore.vue
+++ aladin-gamify/src/components/GetScore.vue
@@ -1,14 +1,14 @@
 <script lang="ts" setup>
-import { ref } from "vue";
+import {ref} from "vue";
 import savedPaths from "../test_data/savedPaths.json";
 import recording from "../test_data/615251051.json";
-import { IReplay, IState, ISteps } from "../../../../CARPET/src/interfaces/TaskGraphInterface.ts";
+import {IReplay, IState, ISteps} from "../../../../CARPET/src/interfaces/TaskGraphInterface.ts";
 
+/**
+ * Removes the numbers from the path, so that the path can be compared with the savedPaths
+ * @param path
+ */
 const normalizePath = (path: string) => path.replaceAll(/__[0-9]/g, "");
-
-
-const relevantSteps: ISteps[] = <IReplay>recording.steps.filter((step: ISteps) => savedPaths.paths.includes(normalizePath(step.path)));
-const score = ref(0);
 
 /**
  * multiply the score for this component with these values e.g. showSolution -> score * 0 = 0
@@ -19,13 +19,10 @@ const impactMethodsToScore = {
   "copyToClipboard": 1,
 };
 
-// Methode mit Paul absprechen ob es so auch generisch genug ist, damit hier dann der Score berechnet werden kann.
-
-const calculateScore = (object: Object) => {
-  let result = 0;
-  score.value = result;
-};
-
+/**
+ * Converts an array of steps to an object, where the path is the key and the value is the value
+ * @param steps
+ */
 const steps2object = (steps: ISteps[]) => {
   let result: Record<string, any> = {};
   steps.forEach((step: ISteps) => {
@@ -38,8 +35,12 @@ const steps2object = (steps: ISteps[]) => {
       const key = keys[i];
 
       if (i === keys.length - 1) {
-        // If it's the last key, assign the value to the property
-        currentObject[key] = step.value;
+        // If it's the last key, add the value along with the timestamp to the property
+        if (!currentObject[key]) {
+          currentObject[key] = {};
+        }
+        currentObject[key].value = step.value;
+        currentObject[key].timestamp = step.timestamp;
       } else {
         // Otherwise, create a nested object and move to the next level
         if (!currentObject[key]) {
@@ -49,28 +50,109 @@ const steps2object = (steps: ISteps[]) => {
       }
     }
   });
-
+  console.log(result)
   return result;
 };
+// ToDO: define more metrics
+const metricsCalculation = {
 
-console.log(steps2object(relevantSteps));
+  // define the time for each node in s (key = node) [should be specific to difficulty from task - can be statistically calculated from user data]
+  timeForNode: {
+    0: undefined,
+    6: 600,
+    7: 1200
+  },
+  // define the maximum score for each node (key = node) [should be specific to difficulty from task]
+  totalScore: {
+    0: 5,
+    6: 10,
+    7: 20
+  }
+}
 
-// TODO: create mapping to define which paths from the object are necessary values for the score
-// Prüfen, muss ich die TimeStamps noch einfügen?
+/**
+ *
+ * @param inputSteps
+ */
+const calculateScore = (inputSteps: ISteps[]) => {
+  // Step 1: Parse the inputSteps into a nested object
+  const parsedData = steps2object(inputSteps);
+
+  // Step 2: Calculate the score for each node
+  const nodeScores: Record<string, number> = {};
+  for (const nodeKey in metricsCalculation.totalScore) {
+    const nodeData = parsedData["nodes"][nodeKey];
+    if (nodeData) {
+      const timeForNode = metricsCalculation.timeForNode[nodeKey];
+      const totalScore = metricsCalculation.totalScore[nodeKey];
+      let componentScores: Record<string, number> = {};
+      for (let componentKey in nodeData["components"]) {
+        const componentData = nodeData["components"][componentKey];
+        if (componentData) {
+          // Score for each component is calculated in percent
+          let componentScore = 1;
+          // TODO
+          // Calculate the score for each component
+          // ... your scoring logic goes here ...
+          componentScores[componentKey] = componentScore
+        }
+      }
+      let nodeScore = Object.keys(componentScores).map((key) => {
+        return componentScores[key];
+      }).reduce((a: number, b: number) => {
+        return (a + b) / 2;
+      })
+      // calculate the absolute score for each node
+      nodeScore *= totalScore;
+      // ToDo: Add timestamp to equation
+      nodeScores[nodeKey] = {
+        components: componentScores,
+        score: nodeScore
+      };
+    }
+  }
+
+  // Step 3: Multiply the calculated score by the impact factor of used methods
+  const finalScores: Record<string, number> = {};
+  for (const nodeKey in nodeScores) {
+    const nodeScore = nodeScores[nodeKey];
+    let finalScore = nodeScore;
+
+    // Apply impact factor if available for each component
+    for (const component in Object.keys(parsedData["nodes"][nodeKey]["components"])) {
+      // Apply impact factor if available
+      const impactMethods: string[] = parsedData["nodes"][nodeKey]["components"][component]?.["contextMenu"]?.["usedMethods"];
+      for (const impactMethod in impactMethods) {
+        if (impactMethod && impactMethodsToScore.hasOwnProperty(impactMethod)) {
+          finalScore *= impactMethodsToScore[impactMethod];
+        }
+      }
+      finalScores[nodeKey] = finalScore;
+    }
+  }
+
+  console.log(finalScores)
+  return finalScores;
+};
+
+const relevantSteps: ISteps[] = <IReplay>recording.steps.filter((step: ISteps) => savedPaths.paths.includes(normalizePath(step.path)));
+const score = ref(0);
+
 </script>
 
 <template>
   <p class="d-inline-flex gap-1">
-    <button class="btn btn-primary" type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample"
-            aria-expanded="false" aria-controls="collapseExample">
+    <button aria-controls="collapseExample" aria-expanded="false" class="btn btn-primary"
+            data-bs-target="#collapseExample"
+            data-bs-toggle="collapse" type="button">
       Show relevant paths
     </button>
-    <button class="btn btn-primary" type="button" @click="() => calculateScore(relevantSteps)">
+    <button class="btn btn-primary" type="button" @click="() => {calculateScore(relevantSteps)}">
       Calculate score
     </button>
     <h4>Score: {{ score }}</h4>
   </p>
-  <div class="collapse show" id="collapseExample">
+  <div id="collapseExample" class="collapse show">
     <div class="card card-body">
       <table>
         <tr>
