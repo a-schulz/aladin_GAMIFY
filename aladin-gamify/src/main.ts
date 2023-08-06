c7db0b6ec01bfbbb70436a1b80d551a1f26ef566
a-schulz <schulz.alexander2002@gmail.com>
2023-08-06 22:55:56 +0200

    added .env to project

--- aladin-gamify/src/main.ts
+++ aladin-gamify/src/main.ts
@@ -1,26 +1,26 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import { SkillsConfiguration } from '@skilltree/skills-client-js';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { SkillsConfiguration } from "@skilltree/skills-client-js";
 
-import App from './App.vue'
-import router from './router'
+import App from "./App.vue";
+import router from "./router";
 
 // Import our custom CSS
-import './scss/styles.scss'
+import "./scss/styles.scss";
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from "bootstrap";
 
 SkillsConfiguration.configure({
-  serviceUrl: 'http://localhost:8080',
-  projectId: 'movies',
-  authenticator: 'http://localhost:8090/api/users/user4@email.com/token',
+  serviceUrl: import.meta.env.VITE_API_URL,
+  projectId: "movies",
+  authenticator: import.meta.env.VITE_AUTHENTICATION_URL + "/api/users/bill@email.org/token",
 });
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 // app.use(SkillsDirective)
 
-app.mount('#app')
+app.mount("#app");
--- aladin-gamify/vite.config.ts
+++ aladin-gamify/vite.config.ts
@@ -1,18 +1,33 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [
+      vue(),
+      vueJsx(),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    server: {
+      proxy: {
+        "/admin": {
+          target: process.env.VITE_API_URL,
+          changeOrigin: true,
+        },
+        "/api/users": {
+          target: process.env.VITE_AUTHENTICATION_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+};
