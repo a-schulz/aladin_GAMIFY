3547e747854f6affbde442218f496baeba057ba0
a-schulz <schulz.alexander2002@gmail.com>
2023-08-07 11:34:41 +0200

    update skill

--- aladin-gamify/src/components/HelloWorldSkillsDisplay.vue
+++ aladin-gamify/src/components/HelloWorldSkillsDisplay.vue
@@ -1,18 +1,33 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import {
   SkillsConfiguration,
   SkillsDisplayJS,
+  SkillsLevelJS,
   SkillsReporter,
 } from "@skilltree/skills-client-js/dist/skills-client-js.esm.min.js";
+import { ref } from "vue";
 
 SkillsConfiguration.afterConfigure().then(() => {
   const clientDisplay = new SkillsDisplayJS();
   clientDisplay.attachTo(document.querySelector("#skills-client-container"));
+  const skillsLevel = new SkillsLevelJS();
+  skillsLevel.attachTo(document.querySelector("#skills-level-container"));
 });
+
+const skillName = ref("");
+
+const reportSkill = () => {
+  if (SkillsConfiguration.isInitialized()) {
+    SkillsReporter.reportSkill(skillName.value);
+  }
+};
 </script>
 
 <template>
+  <input v-model="skillName" type="text" />
+  <button class="btn btn-primary" @click="reportSkill">Add Skill!</button>
   <div id="skills-client-container"></div>
+  <div id="skills-level-container"></div>
 </template>
 
 <style scoped>
